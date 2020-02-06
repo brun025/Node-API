c8cb0ba1421cda4c4873ba33ce5771347da1564c
Bruno <brunofelipedk@gmail.com>
2020-02-05 23:26:43 -0300

    Version 0.0.3

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-global.SALT_KEY = 'SUA SALT_KEY';
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
-    connectionString: 'SUA CONNECTION STRING DO MONGO',
-    sendgridKey: 'SENDGRID KEY PARA EMAIL',
-    containerConnectionString: 'TBD'
+    connectionString: 'mongodb+srv://bruno:Felipe25@0@cluster0-zo3no.azure.mongodb.net/node',
+    sendgridKey: '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',
+    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=node;AccountKey=AlDszbMMoteI3kR3o2LudTHo/2TzqckrazcLzbS83642JP/Po8sC4+0l/hBrS1W0RcLQvgze0hakPgwpb2IhIQ==;EndpointSuffix=core.windows.net'
 }
--- src/models/product.js
+++ src/models/product.js
@@ -34,11 +34,11 @@ const schema = new Schema({
         type: String,
         required: true
     }],
-    // image: {
-    //     type: String,
-    //     required: true,
-    //     trim: true
-    // }
+    image: {
+        type: String,
+        required: true,
+        trim: true
+    }
 });
 
 module.exports = mongoose.model('Product', schema);
