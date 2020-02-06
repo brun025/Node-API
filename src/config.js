e083fc2c897b2e24b073f2d6d111add55ed029c8
brun025 <40203953+brun025@users.noreply.github.com>
2020-02-05 23:30:36 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.SALT_KEY = 'SUA SALT_KEY';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
-    connectionString: 'mongodb+srv://bruno:Felipe25@0@cluster0-zo3no.azure.mongodb.net/node',
-    sendgridKey: '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',
-    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=node;AccountKey=AlDszbMMoteI3kR3o2LudTHo/2TzqckrazcLzbS83642JP/Po8sC4+0l/hBrS1W0RcLQvgze0hakPgwpb2IhIQ==;EndpointSuffix=core.windows.net'
+    connectionString: 'SUA CONNECTION STRING DO MONGO',
+    sendgridKey: 'SENDGRID KEY PARA EMAIL',
+    containerConnectionString: 'SUA CONTAINER CONNECTION STRING DO AZURE'
 }
